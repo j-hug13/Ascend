986c59f1d89cac9dfad176c7a581c2c15f9bcf59
Johnny Hug <johnhug@ksu.edu>
2026-03-16 14:13:05 -0500

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ascend")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ascend.Pages")]
